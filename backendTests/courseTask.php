f31197907359bc1ad3c210df11b301d096d8de1a
hrishd <hrishd@abv.bg>
2023-11-15 10:36:14 +0200

    asd

--- backendTests/courseTask.php
+++ backendTests/courseTask.php
@@ -1,24 +1,36 @@
 <?php
-class CourseTask{
-    private $_functionDeclaration ="";
+class CourseTask
+{
+
+    private $_functionName = "";
+    private $_functionDeclaration = "";
 
     private $_testCases = [];
-    private $_testAnswers =[];
+    private $_testAnswers = [];
 
-    function __construct($functionDeclaration, $testCases, $testAnswers) {
+    function __construct($functionName, $functionDeclaration, $testCases, $testAnswers)
+    {
+        $this->_functionName = $functionName;
         $this->_functionDeclaration = $functionDeclaration;
         $this->_testCases = $testCases;
         $this->_testAnswers = $testAnswers;
     }
 
-    function getFunctionDeclaration() {
+    function getFunnctionName()
+    {
+        return $this->_functionName;
+    }
+    function getFunctionDeclaration()
+    {
         return $this->_functionDeclaration;
     }
 
-    function getTestCases() {
+    function getTestCases()
+    {
         return $this->_testCases;
     }
-    function getTestAnswers() {
+    function getTestAnswers()
+    {
         return $this->_testAnswers;
     }
 }
