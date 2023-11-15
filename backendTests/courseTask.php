c8a4bbbd11f5c316429897df71036d9119b2d2c6
hrishd <hrishd@abv.bg>
2023-11-15 21:41:13 +0200

    asd

--- backendTests/courseTask.php
+++ backendTests/courseTask.php
@@ -8,14 +8,21 @@ class CourseTask
     private $_testCases = [];
     private $_testAnswers = [];
 
+    private $_baseCppFile = "";
+
     function __construct($functionName, $functionDeclaration, $testCases, $testAnswers)
     {
         $this->_functionName = $functionName;
         $this->_functionDeclaration = $functionDeclaration;
         $this->_testCases = $testCases;
         $this->_testAnswers = $testAnswers;
+
+        //Load the file
+        $baseCppFilePath = __DIR__ . "\\..\\rec\\baseCppProgram.txt";
+        $this->_baseCppFile = file_get_contents($baseCppFilePath);
     }
 
+    //Getters
     function getFunnctionName()
     {
         return $this->_functionName;
@@ -32,5 +39,10 @@ class CourseTask
     function getTestAnswers()
     {
         return $this->_testAnswers;
+    }
+
+    function getBaseCppFile()
+    {
+        return $this->_baseCppFile;
     }
 }
