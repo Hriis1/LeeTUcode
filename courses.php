f296eff811dd75e3f41fbeb19e50b52b8cfb43d8
hrishd <hrishd@abv.bg>
2023-11-17 22:37:22 +0200

    moved filtering of courses to different file

--- courses.php
+++ courses.php
@@ -78,24 +78,5 @@
         </div>
     </main>
     <?php include "components/footer.php" ?>
-    <script>
-        // Function to filter courses based on search input
-        function filterCourses() {
-            const searchInput = document.getElementById('searchInput').value.toLowerCase();
-            const courses = document.getElementById('coursesContainer').getElementsByClassName('course-card');
-
-            for (let course of courses) {
-                const courseName = course.querySelector('h3').innerText.toLowerCase();
-
-                if (courseName.includes(searchInput)) {
-                    course.style.display = 'block';
-                } else {
-                    course.style.display = 'none';
-                }
-            }
-        }
-
-        // Attach the filterCourses function to the input event
-        document.getElementById('searchInput').addEventListener('input', filterCourses);
-    </script>
+    <script src="rec/js/filterCourses.js"></script>
 </body>
