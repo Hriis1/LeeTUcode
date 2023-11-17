eb86896335ff86e4762c2d7b7193144363fbf01a
hrishd <hrishd@abv.bg>
2023-11-17 12:05:13 +0200

    seperated head and header into components

---
+++ components/head.php
@@ -0,0 +1,10 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LeeTUcode</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="rec/css/header.css">
+</head>
--- components/header.php
+++ components/header.php
@@ -1,23 +1,11 @@
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LeeTUcode</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
-    <link rel="stylesheet" href="rec/css/header.css">
-</head>
-
-<body>
-    <header>
-        <div class="logo"><a href=""><img src="rec/img/logo-Placeholder.jpg" alt=""></a></div>
-        <div class="heading">
-            <?php
-            $_SESSION["user_id"] = 1;
-            $_SESSION["user_username"] = "zzz";
-            if (isset($_SESSION["user_id"])) {
-                echo '<ul>
+<header>
+    <div class="logo"><a href=""><img src="rec/img/logo-Placeholder.jpg" alt=""></a></div>
+    <div class="heading">
+        <?php
+        $_SESSION["user_id"] = 1;
+        $_SESSION["user_username"] = "zzz";
+        if (isset($_SESSION["user_id"])) {
+            echo '<ul>
 			<li class="user-info">' . $_SESSION["user_username"] . '</li>
             <li>
                 <div class="dropdown">
@@ -34,15 +22,12 @@
              </li>
             <li><a href="include/logout.php" class="under">Отписване</a></li>
 		    </ul>';
-            } else {
-                echo '<ul>
+        } else {
+            echo '<ul>
 			<li><a href="signupForm.php" class="under">Създаване на акаунт</a></li>
 			<li><a href="loginForm.php" class="under">Вписване</a></li>
 		    </ul>';
-            }
-            ?>
-        </div>
-    </header>
-</body>
-
-</html>
+        }
+        ?>
+    </div>
+</header>
