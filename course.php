7be3153a1b8ba74aac8b35b8c67aec6e256dec3c
hrishd <hrishd@abv.bg>
2023-11-18 19:43:01 +0200

    further styled course page

--- course.php
+++ course.php
@@ -4,11 +4,30 @@ include_once "components/head.php";
 ?>
 
 <body>
+    <style>
+        .task-container {
+            display: flex;
+            flex-wrap: wrap;
+        }
+
+        .task-card {
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin: 10px;
+            padding: 20px;
+            width: 200px;
+        }
+
+        .task-card h3 {
+            color: #333;
+        }
+    </style>
     <?php include_once "components/header.php" ?>
     <main>
 
         <div class="container mt-5">
-            <div class="task-info bg-light border border-secondary rounded ps-3 pt-2">
+            <div class="task-info bg-light border border-secondary rounded ps-3 pt-2 pe-3">
                 <div class="row">
                     <div class="col-lg-12">
                         <h2>
@@ -39,27 +58,27 @@ include_once "components/head.php";
                 <div class="row mt-3">
                     <div class="col-lg-12">
                         <h4>Tasks:</h4>
-                        <div class="courses-container" id="coursesContainer">
+                        <div class="task-container d-flex justify-content-center text-center" id="coursesContainer">
                             <!-- Placeholder data, replace with your actual courses -->
-                            <div class="course-card">
-                                <h3>Two Sum</h3>
-                            </div>
-                            <div class="course-card">
+                            <div class="task-card d-flex align-items-center">
                                 <h3>Palindrome Number</h3>
                             </div>
-                            <div class="course-card">
+                            <div class="task-card d-flex align-items-center">
+                                <h3>Palindrome Number</h3>
+                            </div>
+                            <div class="task-card d-flex align-items-center">
                                 <h3>Longest Common Prefix</h3>
                             </div>
-                            <div class="course-card">
+                            <div class="task-card d-flex align-items-center">
                                 <h3>3Sum Closest</h3>
                             </div>
-                            <div class="course-card">
+                            <div class="task-card d-flex align-items-center">
                                 <h3>Remove Duplicates from Sorted Array</h3>
                             </div>
-                            <div class="course-card">
+                            <div class="task-card d-flex align-items-center">
                                 <h3>Search Insert Position</h3>
                             </div>
-                            <div class="course-card">
+                            <div class="task-card d-flex align-items-center">
                                 <h3>Valid Sudoku</h3>
                             </div>
                             <!-- Add more courses as needed -->
