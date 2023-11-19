3a635aeee3929beb51c7d704c3e25ebe655377a4
Velislav-Boyanov <vboy@formicronas.com>
2023-11-19 12:32:10 +0200

    Added funcs for getting table row by Id and fixed my retarded indentation.

--- include/dbHandler.php
+++ include/dbHandler.php
@@ -61,6 +61,23 @@ class dbHandler
 
 
     }
+
+    public function getUserById($id)
+    {
+        $myQuery = $this->mysqli->prepare("SELECT * FROM users WHERE id = ?");
+        $myQuery->bind_param("i", $id);
+        $myQuery->execute();
+
+        $result = $myQuery->get_result();
+
+        // Fetch the first row as an associative array
+        $userArray = $result->fetch_assoc();
+
+        print_r $userArray;
+        $myQuery->close();
+
+        return $userArray;
+    }
     //===============================================TaskSubmition==========================================
     public function createTaskSubmition($submited_function, $submition_status, $task_id, $user_id)
     {
@@ -70,146 +87,211 @@ class dbHandler
         $myQuery->close(); //Free/close the statement   
     }
 
-     public function getTaskSubmition($user_id, $task_id)
-     {
-         $myQuery = $this->mysqli->prepare("SELECT * FROM task_submitions 
-                 WHERE user_id = ? AND task_id = ?");
-         $myQuery->bind_param("ii", $user_id, $task_id);
-         $myQuery->execute();
-         $result = $myQuery->get_result();
+    public function getTaskSubmition($user_id, $task_id)
+    {
+        $myQuery = $this->mysqli->prepare("SELECT * FROM task_submitions 
+                WHERE user_id = ? AND task_id = ?");
+        $myQuery->bind_param("ii", $user_id, $task_id);
+        $myQuery->execute();
+        $result = $myQuery->get_result();
 
-         $taskSubArray = $result->fetch_assoc();
+        $taskSubArray = $result->fetch_assoc();
 
-         print_r $taskSubArray; //these need to be tested because I'm not sure what the result returns actually 
-         $myQuery->close();
+        print_r $taskSubArray; // Fetch the first row as an associative array 
+        $myQuery->close();
 
-         return $taskSubArray;
-     }
+        return $taskSubArray;
+    }
+
+    public function getTaskById($id)
+    {
+        $myQuery = $this->mysqli->prepare("SELECT * FROM task_submitions 
+                WHERE id = ?");
+        $myQuery->bind_param("i", $id);
+        $myQuery->execute();
+        $result = $myQuery->get_result();
+
+        $taskSubArray = $result->fetch_assoc();
+
+        print_r $taskSubArray; // Fetch the first row as an associative array 
+        $myQuery->close();
+
+        return $taskSubArray;
+    }
      //==============================================CourseTask==============================================
-     public function createCourseTask($name, $description, $function_name, $function_declaration, $test_cases, $test_answers, $course_id)
-     {
-         $myQuery = $this->mysqli->prepare("INSERT INTO course_tasks (name, description, function_name, function_declaration, test_cases, test_answers, course_id) VALUES (?,?,?,?,?,?,?);"); //Prepare the sql query
-         $myQuery->bind_param("ssssssi", $name, $description, $function_name, $function_declaration, $test_cases, $test_answers, $course_id); //bind the params in place of the '?'
-         $myQuery->execute(); //Execute the statement
-         $myQuery->close(); //Free/close the statement         
-     }
+    public function createCourseTask($name, $description, $function_name, $function_declaration, $test_cases, $test_answers, $course_id)
+    {
+        $myQuery = $this->mysqli->prepare("INSERT INTO course_tasks (name, description, function_name, function_declaration, test_cases, test_answers, course_id) VALUES (?,?,?,?,?,?,?);"); //Prepare the sql query
+        $myQuery->bind_param("ssssssi", $name, $description, $function_name, $function_declaration, $test_cases, $test_answers, $course_id); //bind the params in place of the '?'
+        $myQuery->execute(); //Execute the statement
+        $myQuery->close(); //Free/close the statement         
+    }
 
-     public function getCourseTasksByCourseId($id)
-     {//Returns all tasks from course
-         $myQuery = $this->mysqli->prepare("SELECT * FROM course_tasks 
-                 WHERE course_id = ?");
-         $myQuery->bind_param("i", $id);
-         $myQuery->execute();
-         $result = $myQuery->get_result();
+    public function getCourseTasksByCourseId($id)
+    {//Returns all tasks from course
+        $myQuery = $this->mysqli->prepare("SELECT * FROM course_tasks 
+                WHERE course_id = ?");
+        $myQuery->bind_param("i", $id);
+        $myQuery->execute();
+        $result = $myQuery->get_result();
          
-         $courseTasksArray = $result->fetch_assoc();
+        $courseTasksArray = $result->fetch_assoc();
 
-         print_r $courseTasksArray; //these need to be tested because I'm not sure what the result returns actually 
-         $myQuery->close();
+        print_r $courseTasksArray; // Fetch the first row as an associative array 
+        $myQuery->close();
 
-         return $courseTasksArray;
-     }
+        return $courseTasksArray;
+    }
 
-     public function getCourseTask($name, $id)
-     {//Get task from course with specific name
-         $myQuery = $this->mysqli->prepare("SELECT * FROM course_tasks 
-                 WHERE course_id = ? AND name = ?");
-         $myQuery->bind_param("is", $id, $name);
-         $myQuery->execute();
-         $result = $myQuery->get_result();
+    public function getCourseTask($name, $id)
+    {//Get task from course with specific name
+        $myQuery = $this->mysqli->prepare("SELECT * FROM course_tasks 
+                WHERE course_id = ? AND name = ?");
+        $myQuery->bind_param("is", $id, $name);
+        $myQuery->execute();
+        $result = $myQuery->get_result();
          
-         $courseTasksArray = $result->fetch_assoc();
+        $courseTasksArray = $result->fetch_assoc();
 
-         print_r $courseTasksArray; //these need to be tested because I'm not sure what the result returns actually 
-         $myQuery->close();
+        print_r $courseTasksArray; // Fetch the first row as an associative array 
+        $myQuery->close();
 
-         return $courseTasksArray;        
-     }
+        return $courseTasksArray;        
+    }
+
+    public function getCourseTaskById($id)
+    {
+        $myQuery = $this->mysqli->prepare("SELECT * FROM course_tasks 
+                WHERE id = ?");
+        $myQuery->bind_param("i", $id);
+        $myQuery->execute();
+        $result = $myQuery->get_result();
+         
+        $courseTasksArray = $result->fetch_assoc();
+
+        print_r $courseTasksArray; // Fetch the first row as an associative array 
+        $myQuery->close();
+
+        return $courseTasksArray; 
+    }
      //=============================================CourseMember=============================================
-     public function createCourseMember($course_id, $user_id)
-     {
-         $myQuery = $this->mysqli->prepare("INSERT INTO course_members (course_id, user_id) VALUES (?,?);"); 
+    public function createCourseMember($course_id, $user_id)
+    {
+        $myQuery = $this->mysqli->prepare("INSERT INTO course_members (course_id, user_id) VALUES (?,?);"); 
          //Prepare the sql query
-         $myQuery->bind_param("ii", $course_id, $user_id); //bind the params in place of the '?'
-         $myQuery->execute(); //Execute the statement
-         $myQuery->close(); //Free/close the statement 
-     }
+        $myQuery->bind_param("ii", $course_id, $user_id); //bind the params in place of the '?'
+        $myQuery->execute(); //Execute the statement
+        $myQuery->close(); //Free/close the statement 
+    }
 
-     public function getCourseMemberByCourse($course_id)
-     {//Gets user_id
-         $myQuery = $this->mysqli->prepare("SELECT user_id FROM course_members 
-                 WHERE course_id = ?");
-         $myQuery->bind_param("i", $course_id);
-         $myQuery->execute();
-         $result = $myQuery->get_result();
+    public function getCourseMemberByCourse($course_id)
+    {//Gets user_id
+        $myQuery = $this->mysqli->prepare("SELECT user_id FROM course_members 
+                WHERE course_id = ?");
+        $myQuery->bind_param("i", $course_id);
+        $myQuery->execute();
+        $result = $myQuery->get_result();
          
-         $courseMembersArray = $result->fetch_assoc();
+        $courseMembersArray = $result->fetch_assoc();
 
-         print_r $courseMembersArray; //these need to be tested because I'm not sure what the result returns actually 
-         $myQuery->close();
+        print_r $courseMembersArray; // Fetch the first row as an associative array 
+        $myQuery->close();
 
-         return $courseMembersArray;
-     }
+        return $courseMembersArray;
+    }
 
-     public function getCourseMemberByUser($user_id)
-     {
-         $myQuery = $this->mysqli->prepare("SELECT course_id FROM course_members 
-                 WHERE user_id = ?");
-         $myQuery->bind_param("i", $user_id);
-         $myQuery->execute();
-         $result = $myQuery->get_result();
+    public function getCourseMemberByUser($user_id)
+    {
+        $myQuery = $this->mysqli->prepare("SELECT course_id FROM course_members 
+                WHERE user_id = ?");
+        $myQuery->bind_param("i", $user_id);
+        $myQuery->execute();
+        $result = $myQuery->get_result();
          
-         $courseMembersArray = $result->fetch_assoc();
+        $courseMembersArray = $result->fetch_assoc();
          
-         print_r $courseMembersArray; //these need to be tested because I'm not sure what the result returns actually 
-         $myQuery->close();
+        print_r $courseMembersArray; // Fetch the first row as an associative array 
+        $myQuery->close();
 
-         return $courseMembersArray;
-     }
+        return $courseMembersArray;
+    }
+
+    public function getCourseMemberById($id)
+    {
+        $myQuery = $this->mysqli->prepare("SELECT * FROM course_members 
+                WHERE id = ?");
+        $myQuery->bind_param("i", $id);
+        $myQuery->execute();
+        $result = $myQuery->get_result();
+         
+        $courseMembersArray = $result->fetch_assoc();
+
+        print_r $courseMembersArray; // Fetch the first row as an associative array 
+        $myQuery->close();
+
+        return $courseMembersArray;        
+    }
      //==============================================Course==================================================
-     public function createCourse($name, $requirements, $description, $creator_id)
-     {
-         $myQuery = $this->mysqli->prepare("INSERT INTO courses (course_id, user_id) VALUES (?,?,?,?);"); 
-         //Prepare the sql query
-         $myQuery->bind_param("sssi", $name, $requirements, $description, $creator_id); 
-         //bind the params in place of the '?'
-         $myQuery->execute(); //Execute the statement
-         $myQuery->close(); //Free/close the statement 
-     }
+    public function createCourse($name, $requirements, $description, $creator_id)
+    {
+        $myQuery = $this->mysqli->prepare("INSERT INTO courses (course_id, user_id) VALUES (?,?,?,?);"); 
+        //Prepare the sql query
+        $myQuery->bind_param("sssi", $name, $requirements, $description, $creator_id); 
+        //bind the params in place of the '?'
+        $myQuery->execute(); //Execute the statement
+        $myQuery->close(); //Free/close the statement 
+    }
 
-     public function getCourseByCreatorId($id)
-     {
-         $myQuery = $this->mysqli->prepare("SELECT * FROM courses 
-                 WHERE creator_id = ?");
-         $myQuery->bind_param("i", $id);
-         $myQuery->execute(); 
-         $result = $myQuery->get_result();
+    public function getCourseByCreatorId($id)
+    {
+        $myQuery = $this->mysqli->prepare("SELECT * FROM courses 
+                WHERE creator_id = ?");
+        $myQuery->bind_param("i", $id);
+        $myQuery->execute(); 
+        $result = $myQuery->get_result();
          
-         $courseArray = $result->fetch_assoc();
+        $courseArray = $result->fetch_assoc();
          
-         print_r $courseArray; //these need to be tested because I'm not sure what the result returns actually 
+        print_r $courseArray; // Fetch the first row as an associative array 
          
-         $myQuery->close();
+        $myQuery->close();
 
-         return $courseArray;
-     }
+        return $courseArray;
+    }
 
-     public function getCourse($id, $name)
-     {//Get course from specific creator with specific name 
-         $myQuery = $this->mysqli->prepare("SELECT * FROM courses 
-                 WHERE creator_id = ? AND name = ?");
-         $myQuery->bind_param("is", $id, $name);
-         $myQuery->execute();
-         $result = $myQuery->get_result();
+    public function getCourse($id, $name)
+    {//Get course from specific creator with specific name 
+        $myQuery = $this->mysqli->prepare("SELECT * FROM courses 
+                WHERE creator_id = ? AND name = ?");
+        $myQuery->bind_param("is", $id, $name);
+        $myQuery->execute();
+        $result = $myQuery->get_result();
          
-         $courseArray = $result->fetch_assoc();
+        $courseArray = $result->fetch_assoc();
          
-         print_r $courseArray; //these need to be tested because I'm not sure what the result returns actually 
+        print_r $courseArray; // Fetch the first row as an associative array 
          
-         $myQuery->close();
+        $myQuery->close();
 
-         return $courseArray;    
-     }
+        return $courseArray;    
+    }
+
+    public function getCourseById($id)
+    {
+        $myQuery = $this->mysqli->prepare("SELECT * FROM courses 
+                WHERE id = ?");
+        $myQuery->bind_param("i", $id);
+        $myQuery->execute();
+        $result = $myQuery->get_result();
+         
+        $courseArray = $result->fetch_assoc();
+         
+        print_r $courseArray; // Fetch the first row as an associative array 
+         
+        $myQuery->close();
+
+        return $courseArray;        
+    }
 
 }
 
