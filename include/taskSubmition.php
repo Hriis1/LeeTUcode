e3cf46a174e15040bdcdf8c0812583dc01e49eae
Velislav-Boyanov <vboy@formicronas.com>
2023-11-20 08:39:42 +0200

    Added task submition class(for now without special funcs).

---
+++ include/taskSubmition.php
@@ -0,0 +1,39 @@
+<?php
+class TaskSubmtion
+{
+
+    private $_submitedFunction = "";
+    private $_submitionStatus = False;
+
+    function __construct($function, $status)
+    {
+        $this->_submitedFunction = $function;
+        $this->_submitionStatus = $status;
+    }
+ 
+    public function setStatus($status)
+    {
+        if(is_bool($status)){
+            $this->_submitionStatus = $status
+        }
+        else
+        {   
+            echo "Status input not boolean!"
+            return -1;
+        }
+    }
+
+    //Getters
+    function getSubmitedFunction()
+    {
+        return $this->_submitedFunction;
+    }
+
+    function getSubmitionStatus()
+    {
+        return $this->_submitionStatus;
+    }
+
+
+
+}
