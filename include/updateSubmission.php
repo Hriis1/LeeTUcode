8435e3009375556324eef99c98d4cd02cc980ae0
hrishd <hrishd@abv.bg>
2023-11-28 13:25:52 +0200

    execute the function

--- include/updateSubmission.php
+++ include/updateSubmission.php
@@ -3,4 +3,5 @@ include_once "dbHandler.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $dbHandler->updateTaskSubmitionStatus($_POST["submitionStatus"], $_POST["taskId"]);
+    $dbHandler->updateTaskSubmitionResponse($_POST["compilationResponse"], $_POST["taskId"]);
 }
