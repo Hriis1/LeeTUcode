b34b033337eadeb0daa088dc8d0e911bf5f56a9f
hrishd <hrishd@abv.bg>
2023-11-28 13:29:21 +0200

    cpp garbo

--- cppTesting/courseTaskTest1/courseTaskTest1/Source.cpp
+++ cppTesting/courseTaskTest1/courseTaskTest1/Source.cpp
@@ -2,45 +2,39 @@
 #include <vector>
 #include <string>
 
-int testFunc(std::vector<int> vec, int x);
+int bustedFunc(int a, int b);
 
 
 int main()
 {
-	if (testFunc({ 1,3,5 }, 2) != 15) {
-		std::cout << "Input: " << "{ 1,3,5 }, 2" << std::endl;
-		std::cout << "Your answer: " << testFunc({ 1,3,5 }, 2) << std::endl;
-		std::cout << "Expected answer: " << 15 << std::endl;
+	if (bustedFunc(1, 2) != 3) {
+		std::cout << "Input: " << "1,2" << std::endl;
+		std::cout << "Your answer: " << bustedFunc(1, 2) << std::endl;
+		std::cout << "Expected answer: " << 3 << std::endl;
 		return 0;
 	}
 
-	if (testFunc({ 1,1,1 }, 2) != 9) {
-		std::cout << "Input: " << "{ 1,1,1 }, 2" << std::endl;
-		std::cout << "Your answer: " << testFunc({ 1,1,1 }, 2) << std::endl;
-		std::cout << "Expected answer: " << 9 << std::endl;
+	if (bustedFunc(2, 5) != 7) {
+		std::cout << "Input: " << "2,5" << std::endl;
+		std::cout << "Your answer: " << bustedFunc(2, 5) << std::endl;
+		std::cout << "Expected answer: " << 7 << std::endl;
 		return 0;
 	}
 
-	if (testFunc({ 2,2,2 }, 5) != 21) {
-		std::cout << "Input: " << "{ 2,2,2 }, 5" << std::endl;
-		std::cout << "Your answer: " << testFunc({ 2,2,2 }, 5) << std::endl;
-		std::cout << "Expected answer: " << 21 << std::endl;
+	if (bustedFunc(5, 5) != 10) {
+		std::cout << "Input: " << "5,5" << std::endl;
+		std::cout << "Your answer: " << bustedFunc(5, 5) << std::endl;
+		std::cout << "Expected answer: " << 10 << std::endl;
 		return 0;
 	}
 
 
 
-	std::cout << "All tests cleared!" << std::endl;
+	std::cout << "All tests cleared!";
 	return 0;
 }
 
-int testFunc(std::vector<int> vec, int x)
+int bustedFunc(int a, int b)
 {
-	int ans = 0;
-	for (size_t i = 0; i < vec.size(); i++)
-	{
-		ans += vec[i] + x;
-	}
-
-	return ans;
+	return a + b;
 }
