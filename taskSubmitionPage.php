bcf95617134b94494d3f6dd042e9d4ac4d949fbc
hrishd <hrishd@abv.bg>
2023-11-29 18:13:56 +0200

    use backup api if codexapi has problem (like now)

--- taskSubmitionPage.php
+++ taskSubmitionPage.php
@@ -39,13 +39,14 @@
         $.ajax(settings).done(function (response) {
             var submitionStatus = "fail";
             var formattedOutput = "";
-            var compilationResponse = ""; 
+            var compilationResponse = "";
+            console.log(response["status"]);
             if (response["output"] != "") {
                 // Replace newline characters with <br> tags
                 formattedOutput = response["output"].replace(/\n/g, '<br>');
 
-                 //Response for the db
-                 compilationResponse = response["output"];
+                //Response for the db
+                compilationResponse = response["output"];
 
                 //Set the submition status
                 if (formattedOutput == "All tests cleared!") {
@@ -72,9 +73,72 @@
                 url: "include/updateSubmission.php",
                 data: { submitionStatus: submitionStatus, taskId: <?php echo $_GET["id"]; ?>, compilationResponse: compilationResponse },
                 success: function (result) {
-                   console.log("update status success!");
+                    console.log("update status success!");
                 }
             });
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            // Handle errors here
+            console.error("Call to Jaagrav CodexAPI failed. Server responded with code: " + jqXHR.status);
+
+            //Call back up api
+            var data = {
+                code: cppFile,
+                input: ""
+            };
+
+            // fetch the API => get response => show response in console
+            fetch("https://compiler.pascaltheelf.workers.dev", {
+                method: 'POST',
+                body: JSON.stringify(data)
+            }).then(res => res.json()).then(data => {
+                showSubmitionResult(data);
+            });
         });
+
+        function showSubmitionResult(response) {
+            var submitionStatus = "fail";
+            var formattedOutput = "";
+            var compilationResponse = "";
+            console.log(response["status"]);
+            if (response["output"] != "") {
+                // Replace newline characters with <br> tags
+                formattedOutput = response["output"].replace(/\n/g, '<br>');
+
+                //Response for the db
+                compilationResponse = response["output"];
+
+                //Set the submition status
+                if (formattedOutput == "All tests cleared!") {
+                    submitionStatus = "success";
+                }
+                else if (!formattedOutput.startsWith("Input:")) {
+                    //If the program did not run correctly
+                    submitionStatus = "error";
+                }
+            }
+            else {
+                //The output that shows to the user
+                formattedOutput = "Program did not compile correctly! <br>" + response["error"];
+
+                //Response for the db
+                compilationResponse = "Program did not compile correctly!\n" + response["error"];
+
+                //Set the submition status
+                submitionStatus = "error";
+            }
+
+            // Set the formatted output in <p> tag
+            $("#codeoutput").html(formattedOutput);
+
+            // Update the submission status in the database using AJAX
+            $.ajax({
+                type: "POST",
+                url: "include/updateSubmission.php",
+                data: { submitionStatus: submitionStatus, taskId: <?php echo $_GET["id"]; ?>, compilationResponse: compilationResponse },
+                success: function (result) {
+                    console.log("update status success!");
+                }
+            });
+        }
     </script>
 </body>
