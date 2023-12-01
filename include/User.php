20abfdd0a5642f652b7c8f1f1a2584c286c5cde8
hrishd <hrishd@abv.bg>
2023-12-01 21:27:05 +0200

    added a user class

---
+++ include/User.php
@@ -0,0 +1,44 @@
+<?php
+include_once "dbHandler.php";
+class User
+{
+    private $_id;
+    private $_username = "";
+    private $_email = "";
+    private $_password = "";
+    private $_account_type = "";
+
+    function __construct($id, $username, $email, $password, $account_type)
+    {
+        $this->_id = $id;
+        $this->_username = $username;
+        $this->_email = $email;
+        $this->_password = $password;
+        $this->_account_type = $account_type;
+    }
+
+    //Getters
+    function getID()
+    {
+        return $this->_id;
+    }
+    function getUsername()
+    {
+        return $this->_username;
+    }
+
+    function getEmail()
+    {
+        return $this->_email;
+    }
+
+    function getPassword()
+    {
+        return $this->_password;
+    }
+
+    function getAccountType()
+    {
+        return $this->_account_type;
+    }
+}
