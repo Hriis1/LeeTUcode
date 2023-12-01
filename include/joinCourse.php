439c97c6663cf072bf00a9bbfde0131e95c3ba8a
hrishd <hrishd@abv.bg>
2023-12-01 23:42:51 +0200

    implemented joincourse.php

--- include/joinCourse.php
+++ include/joinCourse.php
@@ -2,8 +2,18 @@
 include_once "sessionConfig.php";
 $courseID = $_GET["course_id"];
 if (isset($_SESSION["user_id"])) {
-    header("Location: ../course.php?id=" . $courseID . "&joinCourse=success");
+    if ($dbHandler->hasUserJoinedCourse($_SESSION["user_id"], $courseID)) {
+        //If the user has already joined the course
+        header("Location: ../course.php?id=" . $courseID . "&joinCourse=fail");
+        exit();
+    } else {
+        //If the user has not yet joined the course
+        $dbHandler->joinCourse($courseID, $_SESSION["user_id"]);
+
+        header("Location: ../course.php?id=" . $courseID . "&joinCourse=success");
+        exit();
+    }
 } else {
     header("Location: ../course.php?id=" . $courseID . "&joinCourse=fail");
-exit();
+    exit();
 }
