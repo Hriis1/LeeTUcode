af4fbdc67fc8176fecadcc8e436d3084c7f1c660
hrishd <hrishd@abv.bg>
2023-12-01 23:36:55 +0200

    test joincourse

--- include/joinCourse.php
+++ include/joinCourse.php
@@ -1,1 +1,9 @@
 <?php
+include_once "sessionConfig.php";
+$courseID = $_GET["course_id"];
+if (isset($_SESSION["user_id"])) {
+    header("Location: ../course.php?id=" . $courseID . "&joinCourse=success");
+} else {
+    header("Location: ../course.php?id=" . $courseID . "&joinCourse=fail");
+exit();
+}
