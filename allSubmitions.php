9402fa913832c0981a00e8a3abcdb0e457aeeda5
hrishd <hrishd@abv.bg>
2023-12-04 18:12:02 +0200

    added guards for submitions

--- allSubmitions.php
+++ allSubmitions.php
@@ -6,9 +6,14 @@
         <?php
         if ($user != null) {
             //If there is a logged in user
-            
-        }
-        ?>
+            if ($user->hasJoinedCourse($dbHandler, $_GET["course_id"])) {  //If the user is member of the course ?>
+
+            <?php } else {
+                echo "Join the course 1st :)";
+            }
+        } else {
+            echo "Log in 1st :)";
+        } ?>
     </main>
     <?php include_once "components/footer.php" ?>
 </body>
