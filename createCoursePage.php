57819502d8a8ca5578f90d99ac99f993f787dc24
hrishd <hrishd@abv.bg>
2023-12-09 17:36:55 +0200

    safety net for create a course page

--- createCoursePage.php
+++ createCoursePage.php
@@ -3,7 +3,13 @@
 <body>
     <?php include_once "components/header.php" ?>
     <main>
-        Create course page
+        <?php
+        if(isset($user) && $user->getAccountType() == "teacher") { ?>
+
+        gj kopele teacher si
+        <?php } else {
+            echo "You must be a teacher to create a course :)!";
+        } ?>
     </main>
     <?php include_once "components/footer.php" ?>
 </body>
