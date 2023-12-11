e7f0adfa2db78e1e20a1eaaa804d422c57315dd6
hrishd <hrishd@abv.bg>
2023-12-11 23:22:18 +0200

    added form fields for test cases and answers

--- addTestCasesPage.php
+++ addTestCasesPage.php
@@ -41,54 +41,37 @@
             {
                 $_SESSION["add_task_error"] = $error;
 
-                header('Location: addTaskPage.php?course_id='.$course_id.'&createCourse=fail'); //Redirect the user
+                header('Location: addTaskPage.php?course_id=' . $course_id . '&createCourse=fail'); //Redirect the user
                 die();
             }
             ?>
             <div class="container" style="margin-top: 50px;">
                 <div class="form-container border border-secondary rounded p-4">
                     <form id="addTaskForm" action="include/addTestCasesPage.php" method="POST">
-                        TEST CASES BRUV
-                        <div class="d-none">
-                            <input type="number" class="form-control" name="course_id" id="course_id_input"
-                                value="<?php echo $_POST["course_id"]; ?>" required>
-                        </div>
-                        <div class="row mx-1">
-                            <label for="recipient-name" class="col-form-label">Difficulty:</label>
-                            <div class="col-2">
-                                <select name="difficulty" id="difficulty_input" required>
-                                    <option value="easy" selected>Easy</option>
-                                    <option value="medium">Medium</option>
-                                    <option value="hard">Hard</option>
-                                </select>
+                        <?php for ($i = 0; $i < intval($num_tests); $i++) { ?>
+                            <div class="row">
+                                <div class="col-1"></div>
+                                <div class="col-4">
+                                    <label for="recipient-name" class="col-form-label">
+                                        Test case
+                                        <?php echo $i; ?>:
+                                    </label>
+                                    <input type="text" class="form-control" name="test<?php echo $i; ?>"
+                                        id="test<?php echo $i; ?>_input" required>
+                                </div>
+                                <div class="col-2"></div>
+                                <div class="col-4">
+                                    <label for="recipient-name" class="col-form-label">
+                                        Answer
+                                        <?php echo $i; ?>:
+                                    </label>
+                                    <input type="text" class="form-control" name="answer<?php echo $i; ?>"
+                                        id="answer<?php echo $i; ?>_input" required>
+                                </div>
+                                <div class="col-1"></div>
                             </div>
-                        </div>
-                        <div class="row mx-1">
-                            <label for="recipient-name" class="col-form-label">Task name:</label>
-                            <input type="text" class="form-control" name="name" id="name_input" required>
-                        </div>
-                        <div class="row mx-1">
-                            <label for="recipient-name" class="col-form-label">Description:</label> <br>
-                            <textarea name="description" id="description_input" placeholder="Description"
-                                required></textarea>
-                        </div>
-                        <div class="row mx-1">
-                            <label for="recipient-name" class="col-form-label">Function name:</label>
-                            <input type="text" class="form-control" name="func_name" id="func_name_input" required>
-                        </div>
-                        <div class="row mx-1">
-                            <label for="recipient-name" class="col-form-label">Function declaration:</label>
-                            <input type="text" class="form-control" name="func_name" id="func_name_input" required>
-                        </div>
-                        <div class="row mx-1">
-                            <label for="recipient-name" class="col-form-label">Number of tests:</label>
-                            <input type="number" class="form-control" name="num_tests" id="num_tests_input" step="1" min="0"
-                                max="50" required>
-                        </div>
-                        <div class="row mx-1 mb-5">
-                            <label id="add_task_error" class="col-form-label text-danger"></label>
-                        </div>
-                        <input type="Submit" name="submit" value="Add test cases" class="btn btn-primary">
+                        <?php } ?>
+                        <input type="Submit" name="submit" value="Finish task" class="btn btn-primary mt-5">
                     </form>
                 </div>
             </div>
